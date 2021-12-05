3ed4ddb6b1cb72f5bc36d04b600d350bb55eaa02
Rain7 <1789446861@qq.com>
2021-12-05 14:53:39 +0800

    feat: only simple mode in release 1.x

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import { Tabs } from 'antd';
-import { useTranslation } from '../../common/i18n';
 import { SimpleMode } from './SimpleMode';
-import { EnhanceMode } from './EnhanceMode';
 import { MainContainer } from './styles';
-
-const { TabPane } = Tabs;
 
 /**
  * Main
@@ -13,18 +8,9 @@ const { TabPane } = Tabs;
  * TODO: move the generate biz to a unique module
  */
 export const Main = React.memo(() => {
-  const { t } = useTranslation();
-
   return (
     <MainContainer>
-      <Tabs defaultActiveKey="1">
-        <TabPane tab={t('app-mode_simple')} key="1">
-          <SimpleMode />
-        </TabPane>
-        <TabPane tab={t('app-mode_enhance')} key="2">
-          <EnhanceMode />
-        </TabPane>
-      </Tabs>
+      <SimpleMode />
     </MainContainer>
   );
 });
