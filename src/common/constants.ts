9b3d5840f92e0425a757ed09d7a4b929c603828a
Rain7 <1789446861@qq.com>
2021-12-05 13:37:10 +0800

    feat: update dl url

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -16,9 +16,7 @@ export const DEFAULT_GLOBAL_SETTINGS: IGlobalSettings = {
   language: ELanguage.Chinese,
 };
 
-export const CHINESE_DOWNLOAD_URL = 'https://github.com/Mania-Visualization-Project/Mania-Replay-Master/blob/master/README.md';
-export const ENGLISH_DOWNLOAD_URL = 'https://github.com/Mania-Visualization-Project/Mania-Replay-Master/blob/master/README_EN.md';
 export const QQ_GROUP_URL = 'https://jq.qq.com/?_wv=1027&k=lhgX94bn';
 export const FEEDBACK_URL = 'https://github.com/Mania-Visualization-Project/mania-render-website/issues';
-export const DOWNLOAD_ZIP_URL = 'https://mania-replay-master-1305818561.cos.ap-shanghai.myqcloud.com/ManiaReplayMaster%20v2.2.1.zip';
-export const DOWNLOAD_ZIP_FILENAME = 'ManiaReplayMaster v2.2.1.zip';
+export const DOWNLOAD_ZIP_URL = 'https://mania-replay-master-1305818561.cos.ap-shanghai.myqcloud.com/ManiaReplayMaster%20v2.2.2.zip';
+export const DOWNLOAD_ZIP_FILENAME = 'ManiaReplayMaster v2.2.2.zip';
